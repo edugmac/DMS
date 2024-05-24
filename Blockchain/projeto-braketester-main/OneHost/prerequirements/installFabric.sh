04809a2211e61ee447b599b7d35facdd3672a1c2
EduardoV-V <youremail@gmail.com>
2024-05-24 09:38:59 -0300

    Atualizando arquivo de instalação

--- Blockchain/projeto-braketester-main/OneHost/prerequirements/installFabric.sh
+++ Blockchain/projeto-braketester-main/OneHost/prerequirements/installFabric.sh
@@ -22,7 +22,8 @@ sudo apt-get update
 sudo apt-get -y install docker-ce
 
 #install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo rm /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod a+x /usr/local/bin/docker-compose
 
 #install golang
