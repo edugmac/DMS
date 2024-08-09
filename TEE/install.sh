55254e0a9f1d7a11e98ab07b837e967c429b4841
Eduardo Valente <132018113+EduardoV-V@users.noreply.github.com>
2024-08-09 15:57:09 -0300

    Atualizando script de instalação

--- TEE/install.sh
+++ TEE/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir -p logfiles
 TIMESTAMP=$(date +"%d%m%Y_%H%M%S")
 exec 3>&1 1>"logfiles/logfile_${TIMESTAMP}.log" 2>&1
 set -x
@@ -8,12 +9,9 @@ echo "Iniciando instalação da TA">&3
 echo "Aviso: A instalação automática somente funcionará caso o diretório de instalação">&3
 echo "seja igual ao do readme. Caso a instalação tenha sido feita num diretório diferente,">&3
 echo "o processo terá de ser feito manualmente." >&3
-
-# Solicitar confirmação do usuário
 echo "Deseja continuar com a instalação? (s/n): " >&3
 read resposta 
 
-# Verificar a resposta do usuário
 if [[ "$resposta" =~ ^[Ss]$ ]]; then
     echo "Continuando com a instalação..." >&3
 else
@@ -21,24 +19,17 @@ else
     exit 1
 fi
 
-# Definir o diretório atual
 CURRENT_DIR=$(dirname "$0")
-
-# Procurar pela pasta "ola_mundo" no diretório atual
 SOURCE_DIR="$CURRENT_DIR/ola_mundo"
-
-# Verificar se a pasta "ola_mundo" existe no diretório atual
 if [ ! -d "$SOURCE_DIR" ]; then
   echo "A pasta 'ola_mundo' não foi encontrada no diretório atual. Clone o repositório novamente!" >&3
   exit 1
 fi
 
-# Definir caminhos fixos para optee_qemu e optee_rasp na pasta home do usuário
 USER_HOME="/home/$USER"
 OPTEE_QEMU_PATH="$USER_HOME/optee_qemu"
 OPTEE_RASP_PATH="$USER_HOME/optee_rasp"
 
-# Função para mover a pasta "ola_mundo" para "optee_examples" dentro do diretório especificado
 move_to_optee_examples() {
   DEST_DIR="$1/optee_examples"
   
@@ -56,7 +47,6 @@ move_to_optee_examples() {
   fi
 }
 
-# Verificar e mover para optee_qemu/optee_examples
 echo "Verificando instalação do OP-TEE para Qemu" >&3
 if [ -d "$OPTEE_QEMU_PATH" ]; then
   move_to_optee_examples "$OPTEE_QEMU_PATH"
@@ -64,7 +54,6 @@ else
   echo "A instalação do OP-TEE para Qemu não foi encontrada" >&3
 fi
 
-# Verificar e mover para optee_rasp/optee_examples
 echo "Verificando instalação do OP-TEE para Raspberry Pi" >&3
 if [ -d "$OPTEE_RASP_PATH" ]; then
   move_to_optee_examples "$OPTEE_RASP_PATH"
